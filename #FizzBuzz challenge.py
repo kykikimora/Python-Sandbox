18cae078cddf3fb90cf65aff3964f8b44025e0c2
carol savchick <carolmariehinds@gmail.com>
2024-03-27 14:20:16 -0400

    add finished fizzbuzz assignment

--- #FizzBuzz challenge.py
+++ #FizzBuzz challenge.py
@@ -3,3 +3,12 @@
 #If divisible by 3, print Fizz; by 5, print Buzz
 #divisible by both, print FizzBuzz
 
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")    
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)    
