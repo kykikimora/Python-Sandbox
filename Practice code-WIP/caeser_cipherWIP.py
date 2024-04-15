e2fe4d5efaeb5a32880d894449b0b88e0693bd7f
carol savchick <carolmariehinds@gmail.com>
2024-04-15 10:33:27 -0400

    updated sandbox files

---
+++ Practice code-WIP/caeser_cipherWIP.py
@@ -0,0 +1,11 @@
+#caeser cipher assignment
+sentence = input("Please enter a sentence: ")
+
+#This is a 'helper' variable, i think
+#Maybe I can do this more concisely with list comprehension
+result = ""
+
+for char in sentence:
+
+
+print(result)
